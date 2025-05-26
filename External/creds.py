982e48f0bbe07bfc953ccb1561f683813082a4bf
Muzahidur Rahman <63135743+rahman19dec@users.noreply.github.com>
2025-05-26 11:44:42 +0300

    Add files via upload

---
+++ External/creds.py
@@ -0,0 +1,12 @@
+el_map_key = "******" #Zone FI, electricity maps
+
+fingrid_key = "**************************"
+
+db_host = "******"
+db_usr = "*******"
+db_pass = "***************"
+
+
+it_user = "*********************"
+it_pass = "*********"
+it_org = "*******"
