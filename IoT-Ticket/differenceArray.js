7e30a274579beb3b71d3c3a9c0b956de113c926d
Muzahidur Rahman <63135743+rahman19dec@users.noreply.github.com>
2025-06-01 17:39:56 +0300

    Update differenceArray.js

--- IoT-Ticket/differenceArray.js
+++ IoT-Ticket/differenceArray.js
@@ -1,19 +1,39 @@
-{
-  var array = getInputValue("array");
-  var outputArray = [];
-  
-  for (var i = 1; i < array.length; i++) {
-      var prev = array[i - 1];
-      var curr = array[i];
-  
-      if (prev.value !== null && curr.value !== null) {
-          outputArray.push({
-              time: curr.time,
-              value: curr.value - prev.value
-          });
-      }
-  }
+var array = getInputValue("array");
+var grouped = {};
+var outputArray = [];
 
-  setOutputValue("diff array", outputArray);
-
+// External compare function to avoid inline function in loop
+function sortByTimeAsc(a, b) {
+    return new Date(a.time) - new Date(b.time);
 }
+
+// Group items by source
+for (var i = 0; i < array.length; i++) {
+    var item = array[i];
+    var source = item.source;
+
+    if (!grouped[source]) {
+        grouped[source] = [];
+    }
+    grouped[source].push(item);
+}
+
+// Compute deltas, keep original objects
+for (var source in grouped) {
+    var sourceArray = grouped[source];
+
+    sourceArray.sort(sortByTimeAsc); // Sort by time
+
+    for (var i = 1; i < sourceArray.length; i++) {
+        var prev = sourceArray[i - 1];
+        var curr = sourceArray[i];
+
+        if (prev.value !== null && curr.value !== null) {
+            var diffItem = Object.assign({}, curr); // clone original object
+            diffItem.value = curr.value - prev.value;
+            outputArray.push(diffItem);
+        }
+    }
+}
+
+setOutputValue("diff array", outputArray);
